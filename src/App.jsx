cf753b458df971a2837587a1f34592bb191aa0dd
Samad Shukurov <samedzauroglu@gmail.com>
2024-09-09 19:15:20 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+// Hooks 
+import { useEffect, useState } from "react"
+// Components
+import Header from './Components/Header'
+import Hero from './Components/Hero'
+
+function App() {
+  //! DarkMode
+  const [darkMode, setdarkMode] = useState(false)
+  const darkModeToggle = () => {
+    setdarkMode(!darkMode)
+  }
+  useEffect(()=>{
+
+    if (darkMode) {
+      document.querySelector(".App").id = "dark"
+    }
+    else {
+      document.querySelector(".App").id = "light"
+  }
+  },[darkMode])
+
+
+  return (
+    <div className='App' id="light">
+      <Header darkModeToggle={darkModeToggle} />
+      <Hero />
+      Test
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,8 @@
+import "../Component-Styles/Hero.css"
+const Hero = () => {
+  return (
+    <section id="hero">HeroSection</section>
+  )
+}
+
+export default Hero
